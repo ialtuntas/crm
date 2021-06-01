d5032068d6caca1b539863855489c81b0dc4396a
ialtuntas <81922214+ialtuntas@users.noreply.github.com>
2021-06-01 23:11:07 +0300

    fix sourceMap

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,8 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js("resources/js/app.js", "public/js")
     .react()
-    .postCss('resources/css/app.css', 'public/css', [
+    .sourceMaps()
+    .postCss("resources/css/app.css", "public/css", [
         //
     ]);
