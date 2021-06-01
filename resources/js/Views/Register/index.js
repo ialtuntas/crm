00154968243b6e55990a737ae192eaa2def86722
ialtuntas <81922214+ialtuntas@users.noreply.github.com>
2021-06-01 18:45:16 +0300

    added register form validations

--- resources/js/Views/Register/index.js
+++ resources/js/Views/Register/index.js
@@ -1,7 +1,12 @@
+import { Formik } from "formik";
 import React from "react";
+import * as Yup from "yup";
 import { Link } from "react-router-dom";
 
-const Index = () => {
+const Register = () => {
+    const handleSubmit = () => {
+        alert("Tıklandı");
+    };
     return (
         <div className="lr-container">
             <main className="form-signin">
@@ -14,49 +19,122 @@ const Index = () => {
                         height="57"
                     />
                     <h1 className="h3 mb-3 fw-normal">Kayıt Ol</h1>
-                    <div className="form-floating">
-                        <input
-                            type="email"
-                            className="form-control"
-                            id="floatingInput"
-                            placeholder="name"
-                        />
-                        <label htmlFor="floatingInput">İsim</label>
-                    </div>
-                    <div className="form-floating">
-                        <input
-                            type="middle"
-                            className="form-control"
-                            id="floatingInput"
-                            placeholder="name@example.com"
-                        />
-                        <label htmlFor="floatingInput">Email adresi</label>
-                    </div>
-                    <div className="form-floating">
-                        <input
-                            type="middle"
-                            className="form-control"
-                            id="floatingInput"
-                            placeholder="******"
-                        />
-                        <label htmlFor="floatingInput">Şifre</label>
-                    </div>
-
-                    <div className="form-floating">
-                        <input
-                            type="password"
-                            className="form-control"
-                            id="floatingPassword"
-                            placeholder="*******"
-                        />
-                        <label htmlFor="floatingPassword">Şifre Tekrarı</label>
-                    </div>
-                    <button
-                        className="w-100 btn btn-lg btn-primary"
-                        type="submit"
+                    <Formik
+                        initialValues={{
+                            name: "",
+                            email: "",
+                            password: "",
+                            password_confirmation: "",
+                        }}
+                        onSubmit={handleSubmit}
+                        validationSchema={Yup.object().shape({
+                            email: Yup.string()
+                                .email("Email Formatı Hatalı")
+                                .required("Email Zorunludur"),
+                            name: Yup.string().required(" Ad Soyad Zorunludur"),
+                            password: Yup.string().required("Şifre Zorunludur"),
+                            password_confirmation: Yup.string()
+                                .required("Şifre Tekrarı Zorunludur")
+                                .oneOf(
+                                    [Yup.ref("password"), null],
+                                    "Şifreler Eşleşmiyor"
+                                ),
+                        })}
                     >
-                        Kayıt Ol
-                    </button>
+                        {({
+                            values,
+                            handleChange,
+                            handleSubmit,
+                            handleBlur,
+                            errors,
+                            isValid,
+                            isSubmitting,
+                            touched,
+                        }) => (
+                            <div>
+                                <div className="form-floating">
+                                    <input
+                                        type="email"
+                                        className="form-control"
+                                        placeholder="name"
+                                        name="name"
+                                        onBlur={handleBlur}
+                                        placeholder="Ad Soyad"
+                                        value={values.name}
+                                        onChange={handleChange("name")}
+                                    />
+                                    {errors.name && touched.name && (
+                                        <p>{errors.name}</p>
+                                    )}
+                                    <label htmlFor="floatingInput">İsim</label>
+                                </div>
+                                <div className="form-floating">
+                                    <input
+                                        type="middle"
+                                        className="form-control"
+                                        placeholder="name@example.com"
+                                        name="email"
+                                        autoComplete="off"
+                                        onBlur={handleBlur}
+                                        value={values.email}
+                                        onChange={handleChange("email")}
+                                    />
+                                    {errors.email && touched.email && (
+                                        <p>{errors.email}</p>
+                                    )}
+                                    <label htmlFor="floatingInput">
+                                        Email adresi
+                                    </label>
+                                </div>
+                                <div className="form-floating">
+                                    <input
+                                        type="password"
+                                        id="password"
+                                        className="form-control"
+                                        placeholder="******"
+                                        value={values.password}
+                                        onBlur={handleBlur}
+                                        onChange={handleChange("password")}
+                                    />
+                                    {errors.password && touched.password && (
+                                        <p>{errors.password}</p>
+                                    )}
+                                    <label htmlFor="floatingInput">Şifre</label>
+                                </div>
+
+                                <div className="form-floating">
+                                    <input
+                                        type="password"
+                                        id="password_confirmation"
+                                        className="form-control"
+                                        placeholder="*******"
+                                        onBlur={handleBlur}
+                                        value={values.password_confirmation}
+                                        onChange={handleChange(
+                                            "password_confirmation"
+                                        )}
+                                    />
+                                    {errors.password_confirmation &&
+                                        touched.password_confirmation && (
+                                            <p>
+                                                {errors.password_confirmation}
+                                            </p>
+                                        )}
+                                    <label htmlFor="floatingPassword">
+                                        Şifre Tekrarı
+                                    </label>
+                                </div>
+                                <button
+                                    disabled={!isValid || isSubmitting}
+                                    onClick={handleSubmit}
+                                    className="w-100 btn btn-lg btn-primary"
+                                    type="button"
+                                >
+                                    Kayıt Ol
+                                </button>
+                            </div>
+                        )}
+                    </Formik>
                     <Link to="/login">
                         <button className="mt-1 w-100 btn btn-lg btn-outline-danger">
                             Giriş Yap
@@ -69,4 +147,4 @@ const Index = () => {
     );
 };
 
-export default Index;
+export default Register;
