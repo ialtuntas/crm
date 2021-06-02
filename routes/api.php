c9e07df7e9859301b6212eac43bed1b3bd26824f
ialtuntas <81922214+ialtuntas@users.noreply.github.com>
2021-06-03 02:15:21 +0300

    authenticate fonksiyonu

--- routes/api.php
+++ routes/api.php
@@ -26,4 +26,5 @@ Route::group([
     'middleware' => ['auth:api']
 ], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/authenticate', [AuthController::class, 'authenticate']);
 });
