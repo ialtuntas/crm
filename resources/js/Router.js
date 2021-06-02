beb0afeee2cc5c7e993f20ba79ed01d78844cd39
ialtuntas <81922214+ialtuntas@users.noreply.github.com>
2021-06-02 13:23:36 +0300

    Prive route sistemini kurdum

--- resources/js/Router.js
+++ resources/js/Router.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-
+import PriveteRoute from "./PrivateRoute";
 import FrontIndex from "./Views/Index";
 import FrontLogin from "./Views/Login";
 import FrontRegister from "./Views/Register";
 const Main = () => (
     <Switch>
-        <Route exact path="/" component={FrontIndex} />
+        <PriveteRoute exact path="/" component={FrontIndex} />
         <Route path="/Login" component={FrontLogin} />
         <Route path="/Register" component={FrontRegister} />
     </Switch>
