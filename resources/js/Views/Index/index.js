fb05b3924144813e2360b126e6c5bfe8efceadb6
ialtuntas <81922214+ialtuntas@users.noreply.github.com>
2021-06-02 15:01:04 +0300

    Logout kısmı

--- resources/js/Views/Index/index.js
+++ resources/js/Views/Index/index.js
@@ -1,7 +1,18 @@
+import { inject, observer } from "mobx-react";
 import React from "react";
 
-const Index = () => {
-    return <div>Burası index</div>;
+const Index = (props) => {
+    props.AuthStore.getToken();
+    const logout = () => {
+        props.AuthStore.removeToken();
+        props.history.push("/login");
+    };
+    console.log(props.AuthStore.appState.user.id);
+    return (
+        <div>
+            Burası index <button onClick={logout}> Çıkış</button>{" "}
+        </div>
+    );
 };
 
-export default Index;
+export default inject("AuthStore")(observer(Index));
