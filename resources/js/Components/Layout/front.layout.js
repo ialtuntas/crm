495ef8862bba44ab9d17275142045879073a668c
ialtuntas <81922214+ialtuntas@users.noreply.github.com>
2021-06-03 05:30:39 +0300

    Navbar eklendi

--- resources/js/Components/Layout/front.layout.js
+++ resources/js/Components/Layout/front.layout.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { inject, observer } from "mobx-react";
 import React, { useState, useEffect } from "react";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 const Layout = (props) => {
     const [user, setUser] = useState({});
@@ -55,7 +56,38 @@ const Layout = (props) => {
 
     return (
         <>
-            <button onClick={logout}> Çıkış</button>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">BStock</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#features">Yönetim Paneli</Nav.Link>
+                            <Nav.Link href="#pricing">Ürünler</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <NavDropdown
+                                title={user.name}
+                                id="collasible-nav-dropdown"
+                            >
+                                <NavDropdown.Item href="#action/3.1">
+                                    Profil Düzenle
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">
+                                    Şifre Değiştir
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">
+                                    Something
+                                </NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item onClick={logout}>
+                                    Çıkış Yap
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
             <div>{props.children}</div>
         </>
     );
