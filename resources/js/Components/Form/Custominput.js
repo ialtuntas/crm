4876e54edd1c1b6d51f5109c8f1723cfc00b4ff0
ialtuntas <81922214+ialtuntas@users.noreply.github.com>
2021-06-11 06:33:58 +0300

    Update Custominput.js

--- resources/js/Components/Form/Custominput.js
+++ resources/js/Components/Form/Custominput.js
@@ -15,7 +15,6 @@ export default function Custominput({
                 type={type}
                 className="form-control"
                 placeholder={placeholder}
-                name={type}
                 autoComplete="off"
                 value={value}
                 onChange={handleChange}
